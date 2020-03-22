bf5e5eb820f6c502aa25e05f568656ec565e5152
toghrulgasimov <toghrulgasimov@gmail.com>
2020-03-22 19:13:47 +0100

    location changed listener Class

--- app/src/main/java/com/family/familyprotector/MainActivity.java
+++ app/src/main/java/com/family/familyprotector/MainActivity.java
@@ -1,8 +1,13 @@
 package com.family.familyprotector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.provider.Browser;
 import android.util.Log;
@@ -14,13 +19,25 @@ import android.widget.Toast;
 
 import android.os.Bundle;
 
+import com.family.location.LocationService;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    LocationService gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("salam", "alalalal");
         // currently not possible
         //String[] proj = new String[] { Browser., Browser.BookmarkColumns.URL };
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        LocationService L = new LocationService(this);
     }
 }
--- app/src/main/java/com/family/location/LocationService.java
+++ app/src/main/java/com/family/location/LocationService.java
@@ -1,24 +1,72 @@
 package com.family.location;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.TextView;
 
-public class LocationService extends Service {
+public class LocationService implements LocationListener{
 
-    private FusedLocationProviderClient fusedLocationClient;
-    public void getLastLocation() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    protected LocationManager locationManager;
+    protected final Context context;
+
+    public LocationService(Context context) {
+        this.context = context;
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return ;
+        }else{
+            // Write you code here if permission already given.
+            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, this);
+        }
     }
 
-    @Nullable
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onLocationChanged(Location location) {
+        Log.d("salam", location.getLatitude() + "-" + location.getLongitude());
     }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+//    @Nullable
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//
+//        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//            return null;
+//        }else{
+//            // Write you code here if permission already given.
+//            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+//        }
+//        return null;
+//    }
 }
