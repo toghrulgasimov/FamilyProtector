c9eeff1d46e5ff27368a531c44750ac53139f813
toghrulgasimov <toghrulgasimov@gmail.com>
2020-03-22 16:40:26 +0100

    add location permissions and service

--- app/src/main/java/com/family/location/LocationService.java
+++ app/src/main/java/com/family/location/LocationService.java
@@ -1,12 +1,24 @@
 package com.family.location;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
-public class LocationService {
+public class LocationService extends Service {
 
     private FusedLocationProviderClient fusedLocationClient;
     public void getLastLocation() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
