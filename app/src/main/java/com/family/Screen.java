d6062c490147d17dbd03847ebb51e82fa13e34c5
toghrulgasimov <toghrulgasimov@gmail.com>
2020-03-22 19:29:47 +0100

    screenshoot app

--- app/src/main/java/com/family/Screen.java
+++ app/src/main/java/com/family/Screen.java
@@ -3,5 +3,6 @@ package com.family;
 public class Screen {
 
     //https://stackoverflow.com/questions/44288546/take-a-screenshot-background-service
-    
+    // simple app https://github.com/commonsguy/cw-omnibus/tree/FINAL/MediaProjection/andshooter
+
 }
