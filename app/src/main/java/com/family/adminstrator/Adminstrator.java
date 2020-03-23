bef3ade98c88748422bf0932712c1986ab6739ca
toghrulgasimov <toghrulgasimov@gmail.com>
2020-03-23 12:48:26 +0100

    Adminstrator, Notification permissions

---
+++ app/src/main/java/com/family/adminstrator/Adminstrator.java
@@ -0,0 +1,6 @@
+package com.family.adminstrator;
+
+public class Adminstrator {
+    //https://github.com/mitre/device-admin-sample/blob/master/app/src/main/AndroidManifest.xml
+    // i searched there is no need to declare in Manifest
+}
--- app/src/main/java/com/family/notofication/NotificationReader.java
+++ app/src/main/java/com/family/notofication/NotificationReader.java
@@ -2,4 +2,5 @@ package com.family.notofication;
 
 public class NotificationReader {
     //NotificationListenerService
+    //https://stackoverflow.com/questions/44723887/get-notification-access-granted-status
 }
