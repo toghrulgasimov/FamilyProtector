5c1c318396b117f5f009d163785858608d778211
toghrulgasimov <toghrulgasimov@gmail.com>
2020-03-23 23:32:18 +0100

    Battery saving off and allow always run in background permission from setting

--- app/src/main/java/com/family/familyprotector/MainActivity.java
+++ app/src/main/java/com/family/familyprotector/MainActivity.java
@@ -26,6 +26,7 @@ import android.location.LocationListener;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.provider.Browser;
 import android.provider.Settings;
 import android.util.Log;
@@ -147,13 +148,26 @@ public class MainActivity extends AppCompatActivity {
         //setNotificationAccess();
 
 
-        setAdmin();
+        //setAdmin();
 
-        //TODO Wait for Intent
-        // https://developer.android.com/training/basics/intents/result.html
+
+        setBattery();
 
         //TODO Ignore Battery Optimisation. App Always in running
 
+    }
+
+    public void setBattery() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + packageName));
+                startActivity(intent);
+            }
+        }
     }
 
 
