4daa9e30f05c940f1183e7327d7ba4a6286955ea
toghrulgasimov <toghrulgasimov@gmail.com>
2020-04-07 13:23:09 +0200

    addAll

--- app/src/main/java/com/family/familyprotector/Conversation.java
+++ app/src/main/java/com/family/familyprotector/Conversation.java
@@ -1,6 +1,9 @@
 package com.family.familyprotector;
 
+import com.family.util.StringUtil;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Conversation {
     public String name = null;
@@ -25,6 +28,24 @@ public class Conversation {
             if(messages.get(i).time < m.time) {
                 messages.add(i+1, m);
                 return;
+            }
+        }
+    }
+    public void addAll(ArrayList<Message> l) {
+        if(l.size() < 3) return;
+        for(int i = 0; i < messages.size()-2; i++) {
+            if(messages.get(i).content.equals(l.get(0).content) && messages.get(i+1).content.equals(l.get(1).content)&&
+                    messages.get(i+2).content.equals(l.get(2).content)) {
+                for(int j = 3; j < l.size() && i < messages.size(); j++, i++) {
+                    if(!messages.get(i).content.equals(l.get(j).content)) {
+                        Date d = messages.get(i+2).date;
+                        for(int k = j; k < l.size(); k++) {
+                            l.get(k).date = (Date) d.clone();
+                            StringUtil.setDateTime(l.get(k).date, l.get(k).unclear);
+                        }
+                        return;
+                    }
+                }
             }
         }
     }
